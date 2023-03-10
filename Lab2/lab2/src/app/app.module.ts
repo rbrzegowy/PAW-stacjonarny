49e1cdf4120c5b062c8b366d2ee5c66da6825de3
Ryszard Brzegowy <rbrzegowy@wsei.edu.pl>
2023-03-10 14:04:32 +0100

    lab2 - inputy/outputy

--- Lab2/lab2/src/app/app.module.ts
+++ Lab2/lab2/src/app/app.module.ts
@@ -4,12 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { SmallCardComponent } from './small-card/small-card.component';
 import { MediumCardComponent } from './medium-card/medium-card.component';
+import { ProductsComponent } from './products/products.component';
+import { CardComponent } from './card/card.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     SmallCardComponent,
-    MediumCardComponent
+    MediumCardComponent,
+    ProductsComponent,
+    CardComponent
   ],
   imports: [
     BrowserModule
